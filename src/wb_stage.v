a621ba6b1425240f7508e574e5ac3e460f794859
Tawba Abdallah <114433931+tawb@users.noreply.github.com>
2026-01-02 12:20:28 +0200

    Update wb_stage.v

--- src/wb_stage.v
+++ src/wb_stage.v
@@ -3,10 +3,16 @@ module WriteBack(
   input  [31:0] MemOut,
   input  [31:0] NPC3,
   input  [1:0]  WBdata,
-  output [31:0] writeData
+  output reg [31:0] writeData
 );
-  assign writeData = (WBdata == 2'b00) ? ALUout :
-                     (WBdata == 2'b01) ? MemOut :
-                     (WBdata == 2'b10) ? NPC3 :
-                     32'b0;
+
+  always @(*) begin
+    casez (WBdata)
+      2'b00: writeData = ALUout;
+      2'b01: writeData = MemOut;
+      2'b10: writeData = NPC3;
+      default: writeData = 32'b0; // safe NOP
+    endcase
+  end
+
 endmodule
